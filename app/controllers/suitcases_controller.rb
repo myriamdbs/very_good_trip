7ed355a0301caeccd7e4c5f7cd56e1895ecdf7f7
Brad Fujimoto <bradjordan@gmail.com>
2020-05-25 17:26:46 +0200

    added new, create and show actions to suitcases controller

--- app/controllers/suitcases_controller.rb
+++ app/controllers/suitcases_controller.rb
@@ -1,5 +1,36 @@
 class SuitcasesController < ApplicationController
+  before_action :find_suitcase, only: [:show]
+
   def index
     @suitcases = Suitcase.all
   end
+
+  def new
+    @suitcase = Suitcase.new
+  end
+
+  def create
+    @suitcase = Suitcase.new(suitcase_params)
+    @suitcase.user = current_user
+    if @suitcase.save
+      redirect_to suitcases_path
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+private
+
+  def find_suitcase
+    @suitcase = Suitcase.find(params[:id])
+  end
+
+  def suitcase_params
+    params.require(:suitcase).permit(:name, :destination, :start_date, :end_date, :shared, :user)
+  end
+
 end
