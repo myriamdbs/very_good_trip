e4db829badff8dee3f3c3215ade33bdb861843c8
antoine rochon <antoine.rochon@hello-houston.com>
2020-05-26 15:44:49 +0200

    item-new-create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,25 @@
 class ItemsController < ApplicationController
+  
   def new 
-   @suitcase = Item.new
+    @suitcase = Suitcase.find(params[:suitcase_id])
+    @item = Item.new 
+    authorize @item
   end
 
   def create
+    @suitcase = Suitcase.find(params[:suitcase_id])
+    @item = Item.new(item_params)
+    authorize @item
+    @item.suitcase = @suitcase
+    user = @suitcase.user
+    user = current_user
+    if @item.save
+      flash[:notice] = "Your item has been submitted"
+      redirect_to suitcase_path(@suitcase)
+    else
+      @item = Item.new
+      render "suitcases/show"
+    end
   end
 
   def destroy
@@ -12,5 +28,12 @@ class ItemsController < ApplicationController
     @item.destroy
     redirect_to suitcase_path(@item.suitcase)
   end
+
+private
+
+    def item_params
+        params.require(:item).permit(:name)
+    end
+
 
 end
