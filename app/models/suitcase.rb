52f0c681d3d8774bbdc6624d1290c0ab2db84362
Brad Fujimoto <bradjordan@gmail.com>
2020-05-28 15:45:48 +0200

    suitcases index cards redesign and start and end dates made mandatory

--- app/models/suitcase.rb
+++ app/models/suitcase.rb
@@ -2,5 +2,7 @@ class Suitcase < ApplicationRecord
   belongs_to :user
   has_many :items, dependent: :destroy
   validates :destination, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
   has_one_attached :photo
 end
