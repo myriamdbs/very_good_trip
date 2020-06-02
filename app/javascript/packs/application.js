4416dec26a56b2d745d2b17a2d016811fc72c260
Brad Fujimoto <bradjordan@gmail.com>
2020-06-02 11:12:26 +0200

    suggestions button added to interests index and js added to radio buttons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,10 +29,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initAutocomplete } from "../plugins/init_autocomplete";
 import flatpickr from "flatpickr";
+import { toggleChoiceInNewItemForm } from "../components/toggle_choice_in_new_item_form";
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initAutocomplete()
   flatpickr(".datepicker", {});
+  toggleChoiceInNewItemForm();
 });
